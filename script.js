bb7e5c4f01629a76b6bb3f20bebb818ec4acbae6
NickSpringall <123866019+NickSpringall@users.noreply.github.com>
2023-09-08 18:15:09 +1000

    dictionary check

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ getWord().then(data => {
     console.log(data)
     word = data
 })
-
 console.log(word)
 
+
 let guess = document.getElementsByClassName("guess1")
+
 
 var array = []
 for (let index = 0; index < guess.length; index++) {
@@ -21,8 +22,44 @@ for (let index = 0; index < guess.length; index++) {
 
 let counter = 0
 var guessNumber = 1
-let input = document.getElementById("userInput").addEventListener("keydown", (event => {
 
+
+
+async function dictCheck(guessedWord) {
+    let response = await fetch( 'https://api.dictionaryapi.dev/api/v2/entries/en/' + guessedWord);
+    let data = await response.json()
+    return data
+   }
+ 
+
+async function wordCheck(guessedWord) {
+    await dictCheck(guessedWord).then(data => {
+        try {
+        console.log("dictionary says " + data[0].word + " exists")
+        submitGuess(array, word)
+
+            guessNumber ++
+            counter = 0
+    
+            guess = document.getElementsByClassName("guess" + guessNumber)
+            array = []
+    
+            for (let index = 0; index < guess.length; index++) {
+                array.push(guess[index])
+            }
+        return data[0].word
+        }
+
+        catch(error) {
+            console.log(error.message)
+            return null
+        }
+    })
+} 
+
+
+
+let input = document.getElementById("userInput").addEventListener("keydown", (event => {
 
     if (event.key == 'Backspace') {
         array[counter-1].innerHTML = null
@@ -30,23 +67,63 @@ let input = document.getElementById("userInput").addEventListener("keydown", (ev
     } 
     else if (event.key == 'Enter') {
         event.preventDefault()
-        submitGuess(array, word)
 
-        guessNumber ++
-        counter = 0
+        let guessedWord = ""
 
-        guess = document.getElementsByClassName("guess" + guessNumber)
-        array = []
+        array.forEach(element => {
+            guessedWord = guessedWord + element.innerHTML
+        });
+        console.log(array)
+        console.log(guessedWord)
 
-        for (let index = 0; index < guess.length; index++) {
-            array.push(guess[index])
-        }
-    } 
-    else {
-    array[counter].innerHTML = event.key
-    counter ++
+
+        async function wordCheck(guessedWord) {
+            await dictCheck(guessedWord).then(data => {
+                try {
+                console.log("dictionary says " + data[0].word + " exists")
+                submitGuess(array, word)
+        
+                    guessNumber ++
+                    counter = 0
+            
+                    guess = document.getElementsByClassName("guess" + guessNumber)
+                    array = []
+            
+                    for (let index = 0; index < guess.length; index++) {
+                        array.push(guess[index])
+                    }
+                return data[0].word
+                }
+        
+                catch(error) {
+                    console.log(error.message)
+                    alert(guessedWord + " is not a real word!")
+
+                    guess = document.getElementsByClassName("guess" + guessNumber)
+
+                    console.log(array)
+                    array.forEach(element => {
+                        element.innerHTML = ""
+                    });
+
+                    counter = 0
+
+                    return error.message
+
+                }
+            })
+        } 
+        wordCheck(guessedWord)
     }
-}))
+
+    else {
+        array[counter].innerHTML = event.key
+        counter ++
+    }
+
+}));
+
+
 
 
 function submitGuess (array, word) {
@@ -87,47 +164,53 @@ function submitGuess (array, word) {
         }
     }
 
-    // iterate over guess boxes
-    for (let i = 0; i < 5; i++) {
+    function doSetTimeout(i) {
+        setTimeout(function() {
+            colourChange()
+        }, 1000);
 
-        
-        // create variable checking if letter exists and if so,the first index it is in the guess array
-        let ifInWordIndex = wordstr.indexOf(array[i].innerHTML)
 
-        let currentGuessedLetter = array[i].innerHTML
-        let currentCorrectLetter = wordstr[i]
+function colourChange() {
+    // create variable checking if letter exists and if so,the first index it is in the guess array
+    let currentGuessedLetter = array[i].innerHTML
+    let currentCorrectLetter = wordstr[i]
 
-        let currentGuessedLetterNumCorrectGuess = 0
+    let currentGuessedLetterNumCorrectGuess = 0
 
-        for (let x = 0; x < 5; x++) {
-            if ((word[x] == array[x].innerHTML) && (array[x].innerHTML == currentGuessedLetter)) {
-                currentGuessedLetterNumCorrectGuess ++
-            }
+    for (let x = 0; x < 5; x++) {
+        if ((word[x] == array[x].innerHTML) && (array[x].innerHTML == currentGuessedLetter)) {
+            currentGuessedLetterNumCorrectGuess ++
         }
+    }
 
-        let currentCorrectLetterNum = 0
-        for (let x = 0; x < 5; x++) {
-            if (currentGuessedLetter == array[x].innerHTML) {
-                currentCorrectLetterNum ++
-            }
+    let currentCorrectLetterNum = 0
+    for (let x = 0; x < 5; x++) {
+        if (currentGuessedLetter == array[x].innerHTML) {
+            currentCorrectLetterNum ++
         }
+    }
 
-        // check if guessed letter exists in word using -1 returned from indexOf
-        if (ifInWordIndex == -1) {
-            continue;
-        }
-        // check if box has correct guess
-        else if (currentGuessedLetter == currentCorrectLetter) {
-            array[i].style.backgroundColor = "green"
-            continue; 
-        }
+    // check if guessed letter exists in word using -1 returned from indexOf
+    // if (ifInWordIndex == -1) {
+    // }
+    // check if box has correct guess
+    if (currentGuessedLetter == currentCorrectLetter) {
+        array[i].style.backgroundColor = "green"
+        // continue; 
+    }
 
-        else if (letNum[currentGuessedLetter] > 0){
-            array[i].style.backgroundColor = "yellow";
-            letNum[currentGuessedLetter] = letNum[currentGuessedLetter] -1
-            continue;
-        }
+    else if (letNum[currentGuessedLetter] > 0){
+        array[i].style.backgroundColor = "yellow";
+        letNum[currentGuessedLetter] = letNum[currentGuessedLetter] -1
+        // continue;
+    }
 
-        else letNum[currentGuessedLetter] = letNum[currentGuessedLetter] -1
+    else letNum[currentGuessedLetter] = letNum[currentGuessedLetter] -1
+    }
+}
+
+
+for (var i = 0; i < 5; i++) {
+    doSetTimeout(i);
     }
 }
